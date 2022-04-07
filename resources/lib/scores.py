a9064cd3a2f42b2e4193570d5bb0b438c77fc8de
eracknaphobia <eracknaphobia@hotmail.com>
2022-04-06 20:42:31 -0400

    code cleanup

--- resources/lib/scores.py
+++ resources/lib/scores.py
@@ -4,7 +4,8 @@ from datetime import datetime
 import requests
 import os, pytz
 
-class Scores():
+
+class Scores:
 
     def __init__(self):
         self.addon = xbmcaddon.Addon()
@@ -25,7 +26,6 @@ class Scores():
         self.display_milliseconds = 5000
         self.dialog = xbmcgui.Dialog()
         self.monitor = xbmc.Monitor()
-
 
     def toggle_script(self):
         # Toggle the setting
@@ -95,7 +95,8 @@ class Scores():
 
         # Disable spoiler by not showing score notifications for the game the user is currently watching
         if video_playing.find(atcommon.lower()) == -1 and video_playing.find(htcommon.lower()) == -1:
-            self.new_game_stats.append([gid, ateam, hteam, ascore, hscore, gameclock, current_period, desc, headshot])
+            if self.addon.getSetting(id="goal_desc") == 'false' or self.addon.getSetting(id="goal_desc") == 'true' and desc.lower() != 'goal':
+                self.new_game_stats.append([gid, ateam, hteam, ascore, hscore, gameclock, current_period, desc, headshot])
 
     def set_display_ms(self):
         display_seconds = int(self.addon.getSetting(id="display_seconds"))
@@ -110,74 +111,72 @@ class Scores():
         self.display_milliseconds = display_seconds * 1000
 
     def check_if_changed(self, new_item, old_item):
-            # --------------------------
-            # Array key
-            # --------------------------
-            # 0 = game id
-            # 1 = away team
-            # 2 = home team
-            # 3 = away score
-            # 4 = home score
-            # 5 = game clock
-            # 6 = current period
-            # 7 = goal description
-            # 8 = headshot img url
-            # --------------------------
+        # --------------------------
+        # Array key
+        # --------------------------
+        # 0 = game id
+        # 1 = away team
+        # 2 = home team
+        # 3 = away score
+        # 4 = home score
+        # 5 = game clock
+        # 6 = current period
+        # 7 = goal description
+        # 8 = headshot img url
+        # --------------------------
 
-            # If the score for either team has changed and is greater than zero.
-            # Or if the game has just ended show the final score
-            # Or the current peroid has changed
-            if ((new_item[3] != old_item[3]) or (new_item[4] != old_item[4])) or (new_item[5].upper().find('FINAL') != -1 and old_item[5].upper().find('FINAL') == -1) or (new_item[6] != old_item[6]):
+        # If the score for either team has changed and is greater than zero.
+        # Or if the game has just ended show the final score
+        # Or the current period has changed
+        if ((new_item[3] != old_item[8]) or (new_item[4] != old_item[4])) or (new_item[5].upper().find('FINAL') != -1 and old_item[5].upper().find('FINAL') == -1) or (new_item[6] != old_item[6]):
+            # Game variables
+            ateam = new_item[1]
+            hteam = new_item[2]
+            ascore = new_item[3]
+            hscore = new_item[4]
+            gameclock = new_item[5]
+            current_period = new_item[6]
+            desc = new_item[7]
+            headshot = new_item[8]
 
-                # Game variables
-                ateam = new_item[1]
-                hteam = new_item[2]
-                ascore = new_item[3]
-                hscore = new_item[4]
-                gameclock = new_item[5]
-                current_period = new_item[6]
-                desc = new_item[7]
-                headshot = new_item[8]
-
-                notify_mode = ''
-                if new_item[5].upper().find('FINAL') != -1:
-                    # Highlight score of the winning team
-                    notify_mode = 'final'
-                    title = 'Final Score'
-                    if int(ascore) > int(hscore):
-                        message = '[COLOR=' + SCORE_COLOR + ']' + ateam + ' ' + ascore + '[/COLOR]    ' + hteam + ' ' + hscore + '    [COLOR=' + GAMETIME_COLOR + ']' + gameclock + '[/COLOR]'
-                    else:
-                        message = ateam + ' ' + ascore + '    [COLOR=' + SCORE_COLOR + ']' + hteam + ' ' + hscore + '[/COLOR]    [COLOR=' + GAMETIME_COLOR + ']' + gameclock + '[/COLOR]'
-
-                elif new_item[6] != old_item[6]:
-                    # Notify user that the game has started / period has changed
-                    notify_mode = 'game'
-                    title = "Game Update"
-                    message = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '   [COLOR=' + GAMETIME_COLOR + ']' + current_period + ' has started[/COLOR]'
-
+            notify_mode = ''
+            if new_item[5].upper().find('FINAL') != -1:
+                # Highlight score of the winning team
+                notify_mode = 'final'
+                title = 'Final Score'
+                if int(ascore) > int(hscore):
+                    message = '[COLOR=' + self.score_color + ']' + ateam + ' ' + ascore + '[/COLOR]    ' + hteam + ' ' + hscore + '    [COLOR=' + self.gametime_color + ']' + gameclock + '[/COLOR]'
                 else:
-                    # Highlight score for the team that just scored a goal
-                    notify_mode = 'score'
-                    if new_item[3] != old_item[3]: ascore = '[COLOR=' + SCORE_COLOR + ']' + new_item[
-                        3] + '[/COLOR]'
-                    if new_item[4] != old_item[4]: hscore = '[COLOR=' + SCORE_COLOR + ']' + new_item[
-                        4] + '[/COLOR]'
+                    message = ateam + ' ' + ascore + '    [COLOR=' + self.score_color + ']' + hteam + ' ' + hscore + '[/COLOR]    [COLOR=' + self.gametime_color + ']' + gameclock + '[/COLOR]'
 
-                    if self.addon.getSetting(id="goal_desc") == 'false':
-                        title = 'Score Update'
-                        message = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '    [COLOR=' + GAMETIME_COLOR + ']' + gameclock + '[/COLOR]'
-                    else:
-                        title = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '    [COLOR=' + GAMETIME_COLOR + ']' + gameclock + '[/COLOR]'
-                        message = desc
+            elif new_item[6] != old_item[6]:
+                # Notify user that the game has started / period has changed
+                notify_mode = 'game'
+                title = "Game Update"
+                message = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '   [COLOR=' + self.gametime_color + ']' + current_period + ' has started[/COLOR]'
 
-                if self.scoring_updates_on():
-                    img = self.nhl_logo
-                    # Get goal scorers head shot if notification is a score update
-                    if self.addon.getSetting(id="goal_desc") == 'true' and notify_mode == 'score' and headshot != '':
-                        img = headshot
-                    self.dialog.notification(title, message, self.nhl_logo, display_milliseconds, False)
-                    self.monitor.waitForAbort(display_seconds + 5)
+            else:
+                # Highlight score for the team that just scored a goal
+                notify_mode = 'score'
+                if new_item[3] != old_item[3]:
+                    ascore = '[COLOR=' + self.score_color + ']' + new_item[3] + '[/COLOR]'
+                if new_item[4] != old_item[4]:
+                    hscore = '[COLOR=' + self.score_color + ']' + new_item[4] + '[/COLOR]'
 
+                if self.addon.getSetting(id="goal_desc") == 'false':
+                    title = 'Score Update'
+                    message = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '    [COLOR=' + self.gametime_color + ']' + gameclock + '[/COLOR]'
+                else:
+                    title = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '    [COLOR=' + self.gametime_color + ']' + gameclock + '[/COLOR]'
+                    message = desc
+
+            if self.scoring_updates_on():
+                img = self.nhl_logo
+                # Get goal scorers head shot if notification is a score update
+                if self.addon.getSetting(id="goal_desc") == 'true' and notify_mode == 'score' and headshot != '':
+                    img = headshot
+                self.dialog.notification(title, message, img, self.display_milliseconds, False)
+                self.monitor.waitForAbort(self.display_seconds + 5)
 
     def scoring_updates(self):
         todays_date = self.local_to_eastern()
@@ -204,6 +203,8 @@ class Scores():
                         # Break out of loop if updates disabled
                         if not self.scoring_updates_on():
                             break
+                        xbmc.log(str(new_item))
+                        xbmc.log(str(old_item))
                         if new_item[0] == old_item[0]:
                             self.check_if_changed(new_item, old_item)
 
@@ -215,7 +216,6 @@ class Scores():
                         dialog = xbmcgui.Dialog()
                         title = "Score Notifications"
                         self.dialog.notification(title, 'All games have ended, good night.', self.nhl_logo, 5000, False)
-
 
             self.old_game_stats = self.new_game_stats
             self.first_time_thru = 0
